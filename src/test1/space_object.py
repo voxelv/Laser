62d5d5dd7c2441edc4cffc5e7b97d392effc9e93
Tim <legocacherx@gmail.com>
2015-10-27 09:58:51 -0700

    some work with space_objects

--- src/test1/space_object.py
+++ src/test1/space_object.py
@@ -1,15 +1,22 @@
 import laser
+import os
 
 
 class SpaceObject:
 
-    connections = []
+    path = "resources/space_objects/"
 
     def __init__(self, pos):
         self.position = pos
 
+        self.sprite_image_file = "default.png"
+        self.sprite_image_path = os.path.join(self.path, self.sprite_image_file)
+
 
 class ConnectibleSpaceObject(SpaceObject):
+
+    connections = []
+    resource_buffer = 0
 
     def add_connection(self, pos):
         self.connections.append(pos)
