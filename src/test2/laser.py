edf3252d3ab9717013cc8f20b47e29845da26cd0
Tim <legocacherx@gmail.com>
2015-10-28 19:52:33 -0700

    create test2 directory with copy from test1 directory

---
+++ src/test2/laser.py
@@ -0,0 +1,15 @@
+import pygame
+
+
+class Laser:
+    def __init__(self, colors=('red', 'red2', 'white')):
+        self.glow = [pygame.color.Color(color) for color in colors]
+
+    def draw_laser(self, surface, start, stop, width):
+        for color in self.glow:
+            if width <= 0:
+                break
+            pygame.draw.line(surface, color, start, stop, width)
+            width -= 2
+
+
---
+++ src/test2/visuals.py
@@ -0,0 +1,23 @@
+import laser as lsr
+
+
+class Visuals:
+
+    max_width = 700
+    max_height = 500
+    size = (max_width, max_height)
+
+    whiteColor = (255, 255, 255)
+    bgColor = (0, 0, 0)
+    redLaser = lsr.Laser(['red4', 'red', 'white'])
+    blueLaser = lsr.Laser(['blue', 'blue2', 'white'])
+
+    def __init__(self):
+        pass
+
+
+
+
+
+
+
