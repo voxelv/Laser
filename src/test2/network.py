f18969c6f57669c3546170fb5b092c1fe1534923
Tim <legocacherx@gmail.com>
2015-11-02 16:51:47 -0800

    save some work

--- src/test2/network.py
+++ src/test2/network.py
@@ -3,14 +3,14 @@ __author__ = 'Tim'
 
 class Network:
     def __init__(self, net_type):
-        self.objects = []
+        self.objects = NetworkObjectContainer()
         self.connections = []
         self.net_type = net_type
 
     def add_object(self, obj):
         self.objects.append(obj)
 
-    def add_cnxn(self, obj1, obj1_slot, obj2, obj2_slot):
+    def add_connection(self, obj1, obj1_slot, obj2, obj2_slot):
         self.connections.append(NetworkConnection(obj1, obj1_slot, obj2, obj2_slot))
 
 
@@ -19,9 +19,12 @@ class NetworkConnection:
         self.slot1 = obj1.net_slots[obj1_slot]
         self.slot2 = obj2.net_slots[obj2_slot]
 
+    def render_connection(self):
+        pass
+
 
 class NetworkObject:
-    def __init__(self, obj_type, pos):
+    def __init__(self):
         self.ID = "UPDATE ID CODE WITH REGISTRY"
         self.slots = []
 
@@ -30,7 +33,16 @@ class NetworkObject:
 
 
 class Slot:
-    def __init__(self, slot_type, pos, dir):
+    def __init__(self, slot_type, rel_pos, flow_dir):
         self.type = slot_type
-        self.position = pos
-        self.dir = dir
+        self.position = rel_pos
+        self.dir = flow_dir
+
+
+class NetworkObjectContainer(list):
+    def __init__(self, iterable=None):
+        super(NetworkObjectContainer, self).__init__(iterable)
+
+    def render_objects(self):
+        for obj in self:
+            obj.render()
